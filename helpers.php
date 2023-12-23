60970d070f35c4962bf23f8ed6d5353b089670e6
Claudio <callcocam@gmail.com>
2023-12-22 22:54:08 -0300

    model config

--- helpers.php
+++ helpers.php
@@ -9,7 +9,7 @@
 if (!function_exists('get_tenant_id')) {
     function get_tenant_id($tenant = 'tenant_id')
     {
-        if (config('tenant.user', false)) { 
+        if (config('tenant.user', false)) {
             $tenantId = data_get(auth()->user(), $tenant);
             return $tenantId;
         }
@@ -22,6 +22,9 @@ if (!function_exists('get_tenant')) {
     function get_tenant($tenant = 'tenant_id')
     {
         $tenantId = \Callcocam\Tenant\Facades\Tenant::getTenantId($tenant);
-        return \Callcocam\Tenant\Models\Tenant::query()->where('id', $tenantId)->first();
+
+        $model = config('tenant.models.tenant', \Callcocam\Tenant\Models\Tenant::class);
+        
+        return app($model)->query()->where('id', $tenantId)->first();
     }
 }
